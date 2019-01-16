9104f0c54e03ecad93f0ee8641107b4711a23d65
valeria campilongo <cvaleriac@hotmail.it>
2019-01-16 16:03:19 +0000

    Automatically backed up by Learn

--- db/migrate/20190116154947_create_landmarks.rb
+++ db/migrate/20190116154947_create_landmarks.rb
@@ -1,6 +1,6 @@
 class CreateLandmarks < ActiveRecord::Migration
   def change
-    reate_table :landmarks do |t|
+    create_table :landmarks do |t|
       t.string :name
       t.integer :year
       t.integer :figure_id
